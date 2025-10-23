41fc57c7f79401fa02fdd1d552de4a7d8c9d4855
arik509 <sabirhossainarik34@gmail.com>
2025-10-23 16:17:10 +0600

    Added banner

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import baby from "../assets/Play, learn, & grow!.png";
+
+const Banner = () => {
+  return (
+    <div className="relative w-full">
+      <img
+        src={baby}
+        alt="Play, Learn & Grow"
+        className="w-full h-auto object-cover"
+      />
+
+      <div className="absolute inset-0 flex flex-col justify-end items-center pb-12 md:pb-20 bg-black/10">
+        <h2 className="text-white text-3xl md:text-5xl font-bold mb-6 drop-shadow-lg text-center">
+          Play, Learn & Grow!
+        </h2>
+        <button className="bg-[#ff7b54] hover:bg-[#ff5722] text-white font-semibold py-3 px-6 rounded-full transition">
+          Shop Now
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/Layouts/Home.jsx
+++ src/Layouts/Home.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import Navbar from '../Components/Navbar';
-import { Outlet } from 'react-router';
-import Footer from '../Components/Footer';
-import Header from '../Components/Header';
+import React from "react";
+import Navbar from "../Components/Navbar";
+import { Outlet } from "react-router";
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
+import Banner from "../Components/Banner";
 
 const Home = () => {
-    return (
-        <div>
-            <header>
-                <Navbar></Navbar>
-                <Header></Header>
-            </header>
-            <main>
-                <Outlet></Outlet>
-            </main>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div>
+      <header>
+        <Navbar></Navbar>
+        <Header></Header>
+      </header>
+      <main>
+        <Banner></Banner>
+        <Outlet></Outlet>
+      </main>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Home;
